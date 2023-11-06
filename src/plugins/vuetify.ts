ecb5b945f024a588e6ae6a728339905f510480c4
curtbrink <curt.brink@86repairs.com>
2023-11-06 11:18:55 -0500

    Updates lol

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,9 +11,19 @@ import 'vuetify/styles';
 // Composables
 import { createVuetify } from 'vuetify';
 import { mdi } from 'vuetify/iconsets/mdi';
+import {
+  VDataTable,
+  VDataTableServer,
+  VDataTableVirtual,
+} from 'vuetify/labs/VDataTable';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    VDataTable,
+    VDataTableServer,
+    VDataTableVirtual,
+  },
   theme: {
     defaultTheme: 'dark',
     themes: {
